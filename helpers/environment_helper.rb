5fb2f610d7e7fb84adfcd593869871d3fc86190a
Leonardo Mateo <leonardomateo@gmail.com>
2016-04-09 20:25:40 -0300

    Refactors database connection

--- helpers/environment_helper.rb
+++ helpers/environment_helper.rb
@@ -3,15 +3,17 @@ module BaseApp
     def self.init_environment(env)
       self.set_env(env)
 
-      db_params = {
-        'host' => ENV["DATABASE_HOST"],
-        'port' => ENV["DATABASE_PORT"],
-        'user' => ENV["DATABASE_USER"],
-        'password' => ENV["DATABASE_PASS"],
-        'db_name' => ENV["DATABASE_NAME"]
-      }
+      if ENV.key? "DATABASE_HOST"
+        db_params = {
+          'host' => ENV["DATABASE_HOST"],
+          'port' => ENV["DATABASE_PORT"],
+          'user' => ENV["DATABASE_USER"],
+          'password' => ENV["DATABASE_PASS"],
+          'db_name' => ENV["DATABASE_NAME"]
+        }
 
-      Sequel.connect("postgres://#{ENV['DATABASE_USER']}:#{ENV['DATABASE_PASS']}@#{ENV['DATABASE_HOST']}:#{ENV['DATABASE_PORT']}/#{ENV['DATABASE_NAME']}").extension(:pg_array).extension(:pg_json)
+        Sequel.postgres(db_params).extension(:pg_array).extension(:pg_json)
+      end
     end
 
     def self.set_env(env)
