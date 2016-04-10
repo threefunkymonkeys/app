79881822e30748867f16ffb1becc1b40600426aa
Leonardo Mateo <leonardomateo@gmail.com>
2016-04-09 22:30:25 -0300

    Scopes routes under the Routes namespace

--- routes/base.rb
+++ routes/base.rb
@@ -1,7 +1,9 @@
-class BaseRoutes < Cuba
-  define do
-    on get, root do
-      res.write view("pages/home", {}, "layout/home")
+module Routes
+  class Base < Cuba
+    define do
+      on get, root do
+        res.write view("pages/home", {}, "layout/home")
+      end
     end
   end
 end
